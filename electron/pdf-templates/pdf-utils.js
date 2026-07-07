688cce1485aa11246183375a723cb4032f3c8238
Paulo Leite <phvleite@gmail.com>
2026-07-07 11:30:43 -0300

    fix: ajuste formato data e hora no pdf

---
+++ electron/pdf-templates/pdf-utils.js
@@ -0,0 +1,25 @@
+function formatDateBR(dateStr) {
+    if (!dateStr) return "";
+
+    const [yyyy, mm, dd] = dateStr.split("-");
+    if (!yyyy || !mm || !dd) return dateStr;
+
+    return `${dd}/${mm}/${yyyy}`;
+}
+
+function formatCurrencyBR(centavos) {
+    const numberValue = Number(centavos) / 100;
+
+    return new Intl.NumberFormat("pt-BR", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }).format(numberValue);
+}
+
+function formatCurrencyFromDatabase(centavos) {
+    return formatCurrencyBR(centavos);
+}
+
+function formatDateFromDatabase(dateStr) {
+    return formatDateBR(dateStr);
+}
